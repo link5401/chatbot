590d85a3b111f809fcaa3b7b2ff0f456eeee7372
La Duong Xuan Linh <xuanlinh@cubevn.local>
2022-06-16 12:27:26 +0700

    prompt added

--- src/main.go
+++ src/main.go
@@ -9,6 +9,7 @@ func main() {
 	mux := http.NewServeMux()
 	// mux.HandleFunc("/createIntent", createIntent)
 	mux.HandleFunc("/replyIntent", replyIntent)
+
 	// mux.HandleFunc("/about", about)
 	port := ":9000"
 	log.Println("Listening on port ", port)
